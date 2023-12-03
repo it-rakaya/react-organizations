cca1599b8a8e1d4fc9af2b2c52620ad74880abd7
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2023-12-03 10:06:29 +0200

    add search and paginate in facilties

---
+++ src/components/molecules/Search.jsx
@@ -0,0 +1,20 @@
+import SearchIcon from "../atoms/icons/SearchIcon";
+
+/* eslint-disable react/prop-types */
+function Search({ setSearchQuery , placeholder }) {
+  return (
+    <div className="relative w-1/2 ">
+      <input
+        type="text"
+        placeholder={placeholder}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="p-3 w-full rounded-xl border ]"
+      />
+      <div className="absolute left-[10px] top-[12px]">
+        <SearchIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Search;
