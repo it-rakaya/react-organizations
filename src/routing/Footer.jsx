d9c14a6ee2e5260233e8441fd3da17d661b5bfa0
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-01-10 09:33:38 +0200

    footer ==> handle size

--- src/routing/Footer.jsx
+++ src/routing/Footer.jsx
@@ -11,15 +11,13 @@ const Footer = () => {
     : orgData?.organizations?.name_ar;
 
   return (
-    <div
-      className=" w-[100%]  mt-5 rounded-md bg-white dark:bg-darkModeColor footer_shadow"
-    >
+    <div className=" w-[100%]  mt-5 rounded-md bg-white dark:bg-darkModeColor footer_shadow">
       <div className="w-[98%] overflow-hidden m">
-        <div className={` flex  justify-center flex-col md:flex-row items-center  p-2 gap-1`}>
-          <h2 className="text-xs text-black dark:text-white">{t("landing.rights")}</h2>
-          <h2 className="text-xs text-black dark:text-white">
-            {`${t("landing.for")}دى شركة  ركايا 
-            `}
+        <div
+          className={` flex  justify-center flex-col md:flex-row items-center  p-2 gap-1`}
+        >
+          <h2 className="text-sm text-center md:text-base">
+            {t("landing.rights")} لدى شركة ركايا{" "}
             <span className="text-xs text-black dark:text-white">&copy;</span>
             {new Date().getUTCFullYear()}
           </h2>
