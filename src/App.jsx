5776f7b18a10f6b0b1247f8fb9accd220a0e1c2a
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-02-14 10:14:49 +0200

    updates pwa

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import "react-toastify/dist/ReactToastify.css";
 import { useIsRTL } from "./hooks/useIsRTL";
 import { AllRoutesProvider } from "./routing/allRoutes";
 import { registerSW } from "virtual:pwa-register";
+import { UseOrg } from "./context/organization provider/OrganizationProvider";
 const App = () => {
   ///
   const isRTL = useIsRTL();
+  const { orgData } = UseOrg();
+  console.log("🚀 ~ App ~ org:", orgData);
   const updateSW = registerSW({
     onNeedRefresh() {},
     onOfflineReady() {},
@@ -29,7 +32,36 @@ const App = () => {
       document.documentElement.classList.remove("dark");
     }
   }, []);
+  useEffect(() => {
+    var manifestLink = document.querySelector('link[rel="manifest"]');
+    console.log("🚀 ~ useEffect ~ manifestLink:", manifestLink);
+    fetch(manifestLink.href)
+      .then((response) => response.json())
+      .then((manifest) => {
+        manifest.start_url = window.location.href; // تحديث start_url
+        var blob = new Blob([JSON.stringify(manifest)], {
+          type: "application/json",
+        });
+        var newUrl = URL.createObjectURL(blob);
+        manifestLink.href = newUrl;
+      });
+  }, []);
+  useEffect(() => {
+    var manifestLink = document.querySelector('link[rel="manifest"]');
+    fetch(manifestLink.href)
+      .then((response) => response.json())
+      .then((manifest) => {
+        manifest.short_name = orgData?.organizations?.name || "Rakaya";
+        manifest.name = orgData?.organizations?.name || "Rakaya";
+        manifest.start_url = orgData?.organizations?.domain;
 
+        var blob = new Blob([JSON.stringify(manifest)], {
+          type: "application/json",
+        });
+        var newUrl = URL.createObjectURL(blob);
+        manifestLink.href = newUrl;
+      });
+  }, [orgData]);
   return (
     <>
       <AllRoutesProvider />
