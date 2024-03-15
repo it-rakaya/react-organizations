fdc5a76d191c7e14884e2b65718e558c24e57b79
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-03-16 01:16:01 +0200

    test title page

--- src/components/molecules/CustomHead.jsx
+++ src/components/molecules/CustomHead.jsx
@@ -3,10 +3,18 @@ import { UseOrg } from "../../context/organization provider/OrganizationProvider
 
 function CustomHead() {
   const { orgData } = UseOrg();
+  const url = window.location.href;
+  const baseUrl = new URL(url).origin;
+  console.log("🚀 ~ CustomHead ~ baseUrl:", baseUrl);
+  const linkUrl = "https://albaitguests-dev.rmcc.sa/";
 
   return (
     <Helmet>
-      <title>{orgData?.organizations?.name || "Rakaya"}</title>
+      <title>
+        {baseUrl == linkUrl
+          ? "ضيوف البيت"
+          : orgData?.organizations?.name || "Rakaya"}
+      </title>
       <meta
         name="description"
         content={orgData?.organizations?.description || "Default Description"}
