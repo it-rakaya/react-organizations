98b41efd23bc275c14ba1fae28e119c738ef046e
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-06-30 10:30:10 +0300

    update nationalAddress in facility

--- src/components/templates/MyFacilities/NationalAdressData.jsx
+++ src/components/templates/MyFacilities/NationalAdressData.jsx
@@ -1,24 +1,31 @@
 /* eslint-disable react/prop-types */
 
 import { t } from "i18next";
+import { useIsRTL } from "../../../hooks/useIsRTL";
 
 function NationalAdressData({ data, childSection, colorHead, parentSection }) {
+  const isRTL = useIsRTL();
+
   return (
     <div className={parentSection}>
-       <div className="block col-span-2 md:hidden">
-        <h2 className="text-black dark:text-white" >{t("National address")}</h2>
+      <div className="block col-span-2 md:hidden">
+        <h2 className="text-black dark:text-white">{t("National address")}</h2>
       </div>
       <div className={childSection}>
         <p className="font-medium " style={{ color: colorHead }}>
           {t("City")}
         </p>
-        <p className="mt-1 dark:text-white">{data?.city}</p>
+        <p className="mt-1 dark:text-white">
+          {isRTL ? data?.city?.name_ar : data?.city?.name_en}
+        </p>
       </div>
       <div className={childSection}>
         <p className="font-medium " style={{ color: colorHead }}>
           {t("District")}
         </p>
-        <p className="mt-1 dark:text-white">{data?.district}</p>
+        <p className="mt-1 dark:text-white">
+          {isRTL ? data?.district?.name_ar : data?.district?.name_en}
+        </p>
       </div>
       <div className={childSection}>
         <p className="font-medium " style={{ color: colorHead }}>
@@ -26,8 +33,7 @@ function NationalAdressData({ data, childSection, colorHead, parentSection }) {
         </p>
         <p className="mt-1 dark:text-white">{data?.street_name}</p>
       </div>
-    
-     
+
       <div className={childSection}>
         <p className="font-medium " style={{ color: colorHead }}>
           {t("Building Number")}
